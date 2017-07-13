232bea92113b54053ba9d94ef25379af991a1864
AliSinaSaemi <a.s.a.j.2016@gmail.com>
2017-07-13 23:31:27 +0430

    Navbar is compelete but big section and small section are not ready yet so i should fix them

--- content/js/sticky/jquery.sticky.js
+++ content/js/sticky/jquery.sticky.js
@@ -91,7 +91,7 @@
               .css('width', newWidth)
               .css('position', 'fixed')
               .css('top', newTop)
-              .css('z-index', s.zIndex);
+              .css('z-index', '2000');
 
             s.stickyElement.parent().addClass(s.className);
 
@@ -122,13 +122,13 @@
               .css('position', 'absolute')
               .css('top', '')
               .css('bottom', 0)
-              .css('z-index', '');
+              .css('z-index', '2000');
           } else {
             s.stickyElement
               .css('position', 'fixed')
               .css('top', newTop)
               .css('bottom', '')
-              .css('z-index', s.zIndex);
+              .css('z-index', '2000');
           }
         }
       }
@@ -246,7 +246,7 @@
                 'position': '',
                 'top': '',
                 'float': '',
-                'z-index': ''
+                'z-index': '2000'
               })
             ;
           }
