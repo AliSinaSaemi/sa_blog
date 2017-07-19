7fe2c3e1e1cb7fb7fec28ddd3baec0faa712630c
AliSinaSaemi <a.s.a.j.2016@gmail.com>
2017-07-19 22:54:43 +0430

    There is problem that is i can not focus on sidenav only i need help

--- content/js/blog/main.js
+++ content/js/blog/main.js
@@ -3,9 +3,7 @@
 This for make navigation sticky 
 
 */
-$(".navbar").sticky({
-  topSpacing: 0
-});
+$(".navbar").sticky({topSpacing:0});
 
 
 
@@ -14,35 +12,26 @@ $(".navbar").sticky({
 These are for sidenav dropdown animation
 
 */
-(function ($) {
-  $(document).ready(function () {
-    $('#cssmenu li.has-sub>a').on('click', function () {
-      $(this).removeAttr('href');
-      var element = $(this).parent('li');
-      if (element.hasClass('open')) {
-        element.removeClass('open');
-        element.find('li').removeClass('open');
-        element.find('ul').slideUp();
-      } else {
-        element.addClass('open');
-        element.children('ul').slideDown();
-        element.siblings('li').children('ul').slideUp();
-        element.siblings('li').removeClass('open');
-        element.siblings('li').find('li').removeClass('open');
-        element.siblings('li').find('ul').slideUp();
-      }
-    });
-  });
-})(jQuery);
-
-$('.menu-bars').click(function () {
-  $("#cssmenu").addClass('width_nav');
-  $("#cssmenu").removeClass('no_width_nav');
+( function( $ ) {
+$( document ).ready(function() {
+$('#cssmenu li.has-sub>a').on('click', function(){
+		$(this).removeAttr('href');
+		var element = $(this).parent('li');
+		if (element.hasClass('open')) {
+			element.removeClass('open');
+			element.find('li').removeClass('open');
+			element.find('ul').slideUp();
+		}
+		else {
+			element.addClass('open');
+			element.children('ul').slideDown();
+			element.siblings('li').children('ul').slideUp();
+			element.siblings('li').removeClass('open');
+			element.siblings('li').find('li').removeClass('open');
+			element.siblings('li').find('ul').slideUp();
+		}
+	});
 });
+} )( jQuery );
 
 
-
-$('.closebtn').click(function () {
-  $("#cssmenu").addClass('no_width_nav');
-  $("#cssmenu").removeClass('width_nav');
-});
