0c0d6a6ede83f65ced111e7903159feded920577
AliSinaSaemi <a.s.a.j.2016@gmail.com>
2017-09-29 02:07:25 +0800

    Landing done !

--- rtl/content/js/blog/main.js
+++ rtl/content/js/blog/main.js
@@ -52,60 +52,60 @@ $(document).ready(function () {
         collapse
  
 =======================*/
-(function(){
-	var d = document,
-	accordionToggles = d.querySelectorAll('.js-accordionTrigger'),
-	setAria,
-	setAccordionAria,
-	switchAccordion,
-  touchSupported = ('ontouchstart' in window),
-  pointerSupported = ('pointerdown' in window);
-  
-  skipClickDelay = function(e){
+(function () {
+  var d = document,
+    accordionToggles = d.querySelectorAll('.js-accordionTrigger'),
+    setAria,
+    setAccordionAria,
+    switchAccordion,
+    touchSupported = ('ontouchstart' in window),
+    pointerSupported = ('pointerdown' in window);
+
+  skipClickDelay = function (e) {
     e.preventDefault();
     e.target.click();
   }
 
-		setAriaAttr = function(el, ariaType, newProperty){
-		el.setAttribute(ariaType, newProperty);
-	};
-	setAccordionAria = function(el1, el2, expanded){
-		switch(expanded) {
-      case "true":
-      	setAriaAttr(el1, 'aria-expanded', 'true');
-      	setAriaAttr(el2, 'aria-hidden', 'false');
-      	break;
-      case "false":
-      	setAriaAttr(el1, 'aria-expanded', 'false');
-      	setAriaAttr(el2, 'aria-hidden', 'true');
-      	break;
-      default:
-				break;
-		}
-	};
-//function
-switchAccordion = function(e) {
-  console.log("triggered");
-	e.preventDefault();
-	var thisAnswer = e.target.parentNode.nextElementSibling;
-	var thisQuestion = e.target;
-	if(thisAnswer.classList.contains('is-collapsed')) {
-		setAccordionAria(thisQuestion, thisAnswer, 'true');
-	} else {
-		setAccordionAria(thisQuestion, thisAnswer, 'false');
-	}
-  	thisQuestion.classList.toggle('is-collapsed');
-  	thisQuestion.classList.toggle('is-expanded');
-		thisAnswer.classList.toggle('is-collapsed');
-		thisAnswer.classList.toggle('is-expanded');
- 	
-  	thisAnswer.classList.toggle('animateIn');
-	};
-	for (var i=0,len=accordionToggles.length; i<len; i++) {
-		if(touchSupported) {
+  setAriaAttr = function (el, ariaType, newProperty) {
+    el.setAttribute(ariaType, newProperty);
+  };
+  setAccordionAria = function (el1, el2, expanded) {
+    switch (expanded) {
+    case "true":
+      setAriaAttr(el1, 'aria-expanded', 'true');
+      setAriaAttr(el2, 'aria-hidden', 'false');
+      break;
+    case "false":
+      setAriaAttr(el1, 'aria-expanded', 'false');
+      setAriaAttr(el2, 'aria-hidden', 'true');
+      break;
+    default:
+      break;
+    }
+  };
+  //function
+  switchAccordion = function (e) {
+    console.log("triggered");
+    e.preventDefault();
+    var thisAnswer = e.target.parentNode.nextElementSibling;
+    var thisQuestion = e.target;
+    if (thisAnswer.classList.contains('is-collapsed')) {
+      setAccordionAria(thisQuestion, thisAnswer, 'true');
+    } else {
+      setAccordionAria(thisQuestion, thisAnswer, 'false');
+    }
+    thisQuestion.classList.toggle('is-collapsed');
+    thisQuestion.classList.toggle('is-expanded');
+    thisAnswer.classList.toggle('is-collapsed');
+    thisAnswer.classList.toggle('is-expanded');
+
+    thisAnswer.classList.toggle('animateIn');
+  };
+  for (var i = 0, len = accordionToggles.length; i < len; i++) {
+    if (touchSupported) {
       accordionToggles[i].addEventListener('touchstart', skipClickDelay, false);
     }
-    if(pointerSupported){
+    if (pointerSupported) {
       accordionToggles[i].addEventListener('pointerdown', skipClickDelay, false);
     }
     accordionToggles[i].addEventListener('click', switchAccordion, false);
@@ -309,22 +309,6 @@ jQuery(document).ready(function ($) {
     });
   }
 });
-
-
-/*=======================
-
-    show comment box
- 
-=======================*/
-
-/*$(document).ready(function () {
-  $(".comment").click(function (e) {
-    e.preventDefault();
-    $(".comment-inbox").toggleClass("commentBoxShow");
-    $(".submit-comment").toggleClass("commentButtonShow");
-    $(".comment").toggleClass("commentOptionOn");
-  });
-});*/
 
 /*=======================
 
