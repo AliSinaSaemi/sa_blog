82d3b3bc423a8c331d32b69640834062c4e609dc
AliSinaSaemi <a.s.a.j.2016@gmail.com>
2018-03-16 21:37:54 +0330

    New commit

--- rtl/content/js/blog/main.js
+++ rtl/content/js/blog/main.js
@@ -7,6 +7,9 @@ $(window).click(function (e) {
   e.preventDefault();
 });
 
+
+
+
 /*=======================
 
         sticky nav
@@ -37,6 +40,79 @@ $(document).ready(function () {
     $("#searchBoxNav").toggleClass("search-show");
   });
 });
+
+/*=======================
+
+       Back to top
+ 
+=======================*/
+
+$(document).ready(function() {
+  /******************************
+      BOTTOM SCROLL TOP BUTTON
+   ******************************/
+
+  // declare variable
+  var scrollTop = $(".scrollTop");
+
+  $(window).scroll(function() {
+    // declare variable
+    var topPos = $(this).scrollTop();
+
+    // if user scrolls down - show scroll to top button
+    if (topPos > 100) {
+      $(scrollTop).css("opacity", "1");
+
+    } else {
+      $(scrollTop).css("opacity", "0");
+    }
+
+  }); // scroll END
+
+  //Click event to scroll to top
+  $(scrollTop).click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+
+  }); // click() scroll top EMD
+
+  /*************************************
+    LEFT MENU SMOOTH SCROLL ANIMATION
+   *************************************/
+  // declare variable
+  var h1 = $("#h1").position();
+  var h2 = $("#h2").position();
+  var h3 = $("#h3").position();
+
+  $('.link1').click(function() {
+    $('html, body').animate({
+      scrollTop: h1.top
+    }, 500);
+    return false;
+
+  }); // left menu link2 click() scroll END
+
+  $('.link2').click(function() {
+    $('html, body').animate({
+      scrollTop: h2.top
+    }, 500);
+    return false;
+
+  }); // left menu link2 click() scroll END
+
+  $('.link3').click(function() {
+    $('html, body').animate({
+      scrollTop: h3.top
+    }, 500);
+    return false;
+
+  }); // left menu link3 click() scroll END
+
+}); // ready() END
+
+
 /*=======================
 
         hide/ show
@@ -219,26 +295,6 @@ $('#carouselExample').on('slide.bs.carousel', function (e) {
 });
 
 
-/*=======================
-
-      back to top
- 
-=======================*/
-// ===== Scroll to Top ==== 
-$(window).scroll(function () {
-  if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
-    $('#return-to-top').fadeIn(200); // Fade in the arrow
-  } else {
-    $('#return-to-top').fadeOut(200); // Else fade out the arrow
-  }
-});
-$('#return-to-top').click(function () { // When arrow is clicked
-  $('body,html').animate({
-    scrollTop: 0 // Scroll to top of body
-  }, 500);
-});
-
-
 
 /*=======================
 
@@ -301,6 +357,40 @@ $(document).ready(function () {
         items: 1
       },
       600: {
+        items: 1
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });  
+  $('.owl-three').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
+  $('.owl-four').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
         items: 1
       },
       1000: {
